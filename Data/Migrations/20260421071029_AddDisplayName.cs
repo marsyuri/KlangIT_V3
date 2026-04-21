7fdc51c075098bb875099caece1e825bdfb5078b
marsyuri <marsyuri@gmail.com>
2026-04-21 14:11:52 +0700

    add migration สำหรับ DB Identity

---
+++ Data/Migrations/20260421071029_AddDisplayName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlangIT_V3.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}
